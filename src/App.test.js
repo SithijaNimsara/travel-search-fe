edcbd938ee16edcbb71aa4f8cae7b202455b96c8
Sithija <sithija9923@gamil.com>
2024-06-06 16:33:33 +0530

    specific 'Sign In' text within the 'sign-in' id

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Sign In component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/sign in/i);
+  const linkElement = screen.getByText(/sign in/i, { selector: '#sign-in' });
   expect(linkElement).toBeInTheDocument();
 });
